84e6e9b8e6ffb885fad6d595f8e1f5aeaf4c1d86
vladar21 <vladar21@gmail.com>
2021-02-12 01:13:38 +0200

    next 15

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,138 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  {{great}} {{name}}
+  </div>
+
+
+
+<!-- <div v-html="channel"></div>
+<h2 :id="headingId">Heading</h2>
+<button :disabled="isDisable">Bind</button>
+<h2 class="underline">Underlined text</h2>
+<h2 class="underline" :class="status">Status</h2>
+<h2 :class="isPromoted && 'promoted'">Promoted Movie</h2>
+<h2 :class="isSoldout ? 'sold-out' : 'new'">Soldout? movie</h2>
+<h2 :class="['new', 'promoted']">Newly promoted movie</h2>
+<h2 :class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">Array conditional movie</h2>
+<h2 :class="{
+  promoted: isPromoted,
+  new: !isSoldout,
+  'sold-out': isSoldout
+}">Object conditional movie</h2>
+<h2 :style="{
+  color: highlightColor,
+  fontSize: headerSize + 'px',
+  padding: '20px'
+}">Inline Style</h2>
+
+<h2 :style="headerStyleObject">Style Object</h2>
+
+<div :style="[baseStyleObject, successStyleObject]">Success Style</div>
+<div :style="[baseStyleObject, dangerStyleObject]">Danger Style</div> -->
+
+<h2 v-if="num === 0">
+  The number is zero
+  </h2>
+<h2 v-else-if="num < 0">
+  The number is negative
+  </h2>
+<h2 v-else-if="num > 0">
+  The number is positive
+  </h2>
+<h2 v-else>
+  Not a number
+  </h2>
+
+  <template v-if="displey">
+    <h2>Vishwas</h2>
+    <h2>Codevolution</h2>
+    <h2>Vue</h2>
+    <h2 v-show="showElement">Using v-show</h2>
+    <h2 v-if="showElement">Using v-if</h2>
+  </template>
+
+
+  <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
+
+  <h2 v-for="(name, index) in fullNames" :key="name.last">{{ index }}{{name.first}} {{name.last}}</h2>
+
+  <div v-for="actor in actors" :key="actor.name">
+    <h2>{{actor.name}}</h2>
+    <h3 v-for="movie in actor.movies" :key="movie">{{ movie }}</h3>
+
+  </div>
+  <h2 v-for="(value, key, index) in myInfo" :key="value"> {{ index }} {{ key }} {{ value }}</h2>
+  <template v-for="name in names" :key="name">
+    <h2>{{ name }}</h2>
+     <hr />
+
+  </template>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data(){
+    return {
+
+      great: "Hello",
+      name: "Batman",
+      num: 'c',
+      displey: true,
+      showElement: true,
+      names: ['Bruce', 'Clark', 'Diana'],
+      fullNames: [
+        { first: 'Bruce', last: 'Wayne' },
+        { first: 'Clark', last: 'Kent' },
+        { first: 'Princess', last: 'Diana' },
+      ],
+      actors: [
+        {
+          name: "Christian Bale",
+          movies: ['Batman', 'The Prestige'],
+        },
+        {
+          name: "Di Caprio",
+          movies: ['Titanic', 'Inception'],
+        },
+      ],
+      myInfo: {
+        name: 'Vishwas',
+        channel: 'Codevolution',
+        course: 'Vue 3',
+      }
+      // channel: "<b>Codevolution</b>",
+      // headingId: "heading",
+      // isDisable: false,
+      // status: 'danger',
+      // isPromoted: true,
+      // isSoldout: true,
+      // highlightColor: 'orange',
+      // headerSize: 50,
+      // headerStyleObject: {
+      //   color: 'orange',
+      //   fontSize: '50px',
+      //   padding: '20px',
+      // },
+      // baseStyleObject: {
+      //   fontSize: '50px',
+      //   padding: '10px',
+      // },
+      // successStyleObject: {
+      //   color: 'green',
+      //   backgroundColor: 'lightgreen',
+      //   border: '1px solid green',
+      //   padding: '50px'
+      // },
+      // dangerStyleObject: {
+      //   color: 'darkred',
+      //   backgroundColor: 'red',
+      //   border: '1px solid darkred',
+      // }
+    };
+  },
+};
 </script>
 
 <style>
@@ -23,4 +144,21 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+/* .underline {
+  text-decoration: underline;
+}
+
+.promoted {
+  font-style:italic;
+}
+
+.new {
+  color: olivedrab;
+}
+
+.sold-out {
+  color: red;
+} */
+
 </style>
